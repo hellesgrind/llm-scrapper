717429270eb5473e0316a4d730c112d0db7fccdd
hellesgrind <alekseis.naumov@gmail.com>
2024-04-13 21:22:53 +0200

    draft: page scrapping

---
+++ app/prompts.py
@@ -0,0 +1,21 @@
+from typing import List, Dict
+
+
+def scrape_page_prompt(page_text: str, scrapper_schemas: List[Dict]) -> str:
+    prompt = """
+You are an AI expert specializing in knowledge graph creation with the goal of capturing relationships based on a given input or request.
+Knowledge graph is a Dict with 'nodes' and 'relationships' keys.
+You will be given a text and a schema based on which you should extract nodes and relationship.
+Your task is to create a knowledge graph based on the input.
+Extract nodes and relationships between nodes.
+Here's an example:
+Input text:
+Alex is a Software Engineer at Google. Maria gained a bachelor of Physics at MIT.
+Here's a schema for extraction:
+[{'node_type': 'Person', 'properties': ['name']}, {'node_type': 'Position', 'properties': ['position_name', 'company_name']}, {'node_type': 'Education', 'properties': ['school_name']}]
+Correct answer:
+'{"nodes":[{"temp_id":1,"node_type":"Person","name":"Alex"},{"temp_id":2,"node_type":"Person","name":"Maria"},{"temp_id":3,"node_type":"Position","position_name":"Software Engineer","company_name":"Google"},{"temp_id":4,"node_type":"Education","school_name":"MIT"}],"relationships":[{"from_temp_id":1,"to_temp_id":3},{"from_temp_id":2,"to_temp_id":4}]'
+"""  # noqa: E501
+    prompt += f"\nInput text:\n{page_text}"
+    prompt += f"\nHere's a schema for extraction:\n{scrapper_schemas}"
+    return prompt
--- app/scrapper.py
+++ app/scrapper.py
@@ -1,27 +1,58 @@
-from typing import Dict
+import json
+from typing import Dict, List
 
 from parser import Page
 from llm_client import OpenAIModel
 from logs import logger
+from data_manager import DataManager
+from prompts import scrape_page_prompt
 
 
 class PageScrapper:
     def __init__(
         self,
         model: OpenAIModel,
+        scrapper_schemas: List[Dict],
     ):
         self.model = model
+        self.scrapper_schemas = scrapper_schemas
+
+    async def scrape_page(self, page: Page) -> List[Dict]:
+        page_text = page.body_text
+        logger.info(page_text)
+        logger.info(self.scrapper_schemas)
+        messages = [
+            {
+                "role": "user",
+                "content": scrape_page_prompt(
+                    page_text=page_text, scrapper_schemas=self.scrapper_schemas
+                ),
+            },
+        ]
+        response_data = await self.model.generate(messages)
+        response_data = json.loads(response_data)
+        return response_data
+
+    # TODO:
+    async def get_relevant_subpages(self, page: Page) -> List[str]:
+        pass
 
 
 class ScrapperPipeline:
     def __init__(
         self,
         model: OpenAIModel,
-        scrapper_schema: Dict,
+        data_manager: DataManager,
+        scrapper_schemas: List[Dict],
     ):
-        self.model = model
-        self.scrapper_schema = scrapper_schema
+        self.pages: List[Page] | None = []
+        self.data_manager = data_manager
+        self.scrapper = PageScrapper(
+            model=model,
+            scrapper_schemas=scrapper_schemas,
+        )
 
     async def run(self, url: str):
         page = await Page.parse(url)
-        logger.info(page.body_text)
+        response_data = await self.scrapper.scrape_page(page)
+        logger.info(response_data)
