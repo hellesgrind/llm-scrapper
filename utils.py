93d63eb874d8a6d1938c32afd1e325f21c8b1bce
hellesgrind <alekseis.naumov@gmail.com>
2024-04-13 21:22:39 +0200

    send multiple schemas

--- utils.py
+++ utils.py
@@ -2,13 +2,13 @@ from pydantic import BaseModel
 from typing import Type, Dict
 
 
-def generate_json_schema(model: Type[BaseModel]) -> Dict:
+def prepare_schema(model: Type[BaseModel]) -> Dict:
     schema = model.model_json_schema()
-    simplified_properties = {}
+    simplified_properties = []
     for prop, details in schema.get("properties", {}).items():
-        simplified_properties[prop] = {"type": details["type"]}
+        simplified_properties.append(prop)
     simplified_schema = {
-        "title": schema.get("title", model.__name__),
+        "node_type": schema.get("title", model.__name__),
         "properties": simplified_properties,
     }
     return simplified_schema
