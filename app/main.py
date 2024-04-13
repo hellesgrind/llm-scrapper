50dbea1b48a3a5e1653d8fe2edd398487c4f75eb
hellesgrind <alekseis.naumov@gmail.com>
2024-04-13 16:19:12 +0200

    draft: ScrapperPipeline

--- app/main.py
+++ app/main.py
@@ -3,13 +3,22 @@ from logs import logger
 
 from database import Neo4jIntegration
 from schema import ScrapperPayload
+from llm_client import OpenAIModel
+from scrapper import ScrapperPipeline
 
 
 app = FastAPI()
 
 db_integration = Neo4jIntegration()
+scrapper_model = OpenAIModel(model_name="gpt-3.5-turbo")
 
 
 @app.post("/scrapper")
 async def scrapper(payload: ScrapperPayload):
-    logger.info(f"Received payload: {payload}")
+    logger.info(payload.url)
+    logger.info(payload.scrapper_schema)
+    pipeline = ScrapperPipeline(
+        model=scrapper_model,
+        scrapper_schema=payload.scrapper_schema,
+    )
+    await pipeline.run(url=payload.url)
