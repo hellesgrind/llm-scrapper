f5ef1292e24e9e83db50573b497df1ef1b0a8391
hellesgrind <alekseis.naumov@gmail.com>
2024-04-14 16:05:52 +0200

    models for node and edge ingestion to DB

--- app/schema.py
+++ app/schema.py
@@ -7,19 +7,34 @@ class ScrapperPayload(BaseModel):
     scrapper_schemas: List[Dict]
 
 
-class Node(BaseModel):
+class PageNode(BaseModel):
     temp_id: int
     node_type: str
     source: str
     properties: Dict
 
 
-class Edge(BaseModel):
+class PageEdge(BaseModel):
+    type: str = "CONNECTED"
     from_temp_id: int
     to_temp_id: int
     source: str
 
 
 class PageGraph(BaseModel):
-    nodes: List[Node]
-    edges: List[Edge]
+    nodes: List[PageNode]
+    edges: List[PageEdge]
+
+
+class NodeInfo(BaseModel):
+    uuid: str
+    type: str
+    source: str
+    properties: Dict
+
+
+class EdgeInfo(BaseModel):
+    from_uuid: str
+    to_uuid: str
+    type: str
+    source: str
