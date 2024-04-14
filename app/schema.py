60ff0671c98280145ce01bc5c9076451b6984f81
hellesgrind <alekseis.naumov@gmail.com>
2024-04-14 12:36:33 +0200

    implement ResponseProcessor

--- app/schema.py
+++ app/schema.py
@@ -5,3 +5,21 @@ from typing import List, Dict
 class ScrapperPayload(BaseModel):
     url: str
     scrapper_schemas: List[Dict]
+
+
+class Node(BaseModel):
+    temp_id: int
+    node_type: str
+    source: str
+    properties: Dict
+
+
+class Edge(BaseModel):
+    from_temp_id: int
+    to_temp_id: int
+    source: str
+
+
+class PageGraph(BaseModel):
+    nodes: List[Node]
+    edges: List[Edge]
--- app/scrapper.py
+++ app/scrapper.py
@@ -1,11 +1,93 @@
 import json
 from typing import Dict, List
 
-from parser import Page
+from data_manager import DataManager
 from llm_client import OpenAIModel
 from logs import logger
-from data_manager import DataManager
+from parser import Page
 from prompts import scrape_page_prompt
+from schema import (
+    Node,
+    Edge,
+    PageGraph,
+)
+
+
+class ResponseProcessor:
+
+    def process(self, response: str, page_data: Page) -> PageGraph:
+        logger.info(f"Start processing response:{response}")
+        response_data = json.loads(response)
+        nodes = self.create_nodes(
+            response_data=response_data,
+            page_data=page_data,
+        )
+        edges = self.create_edges(
+            response_data=response_data,
+            page_data=page_data,
+        )
+        logger.info("Finish processing response")
+        return PageGraph(nodes=nodes, edges=edges)
+
+    @staticmethod
+    def create_nodes(
+        response_data: Dict,
+        page_data: Page,
+    ) -> List[Node]:
+        nodes: List[Node] = []
+        nodes_data = response_data.get("nodes", [])
+        if not nodes_data:
+            logger.info(
+                f"Unable to get 'nodes' from response: {response_data}",
+            )
+            return nodes
+        for data in nodes_data:
+            temp_id = data.pop("temp_id", None)
+            node_type = data.pop("node_type", None)
+            if not temp_id or not node_type:
+                logger.warning(
+                    f"Unable to get 'temp_id' or 'node_type' of node: {data}"
+                )
+                continue
+            node = Node(
+                temp_id=temp_id,
+                node_type=node_type,
+                properties=data,
+                source=page_data.url,
+            )
+            nodes.append(node)
+            logger.info(f"Extracted 'node': {node}")
+        return nodes
+
+    @staticmethod
+    def create_edges(
+        response_data: Dict,
+        page_data: Page,
+    ) -> List[Edge]:
+        edges: List[Edge] = []
+        edges_data = response_data.get("relationships", [])
+        if not edges_data:
+            logger.info(
+                f"Unable to get 'edges' from response: {response_data}",
+            )
+            return edges
+        for data in edges_data:
+            from_temp_id = data.pop("from_temp_id", None)
+            to_temp_id = data.pop("to_temp_id", None)
+            if not from_temp_id or not to_temp_id:
+                logger.warning(
+                    f"Unable to get 'from_temp_id' "
+                    f"or 'from_temp_id' of edge: {data}"
+                )
+                continue
+            edge = Edge(
+                from_temp_id=from_temp_id,
+                to_temp_id=to_temp_id,
+                source=page_data.url,
+            )
+            edges.append(edge)
+            logger.info(f"Extracted 'edge': {edge}")
+        return edges
 
 
 class PageScrapper:
@@ -16,8 +98,10 @@ class PageScrapper:
     ):
         self.model = model
         self.scrapper_schemas = scrapper_schemas
+        self.response_processor = ResponseProcessor()
 
-    async def scrape_page(self, page: Page) -> List[Dict]:
+    async def scrape_page(self, page: Page) -> PageGraph:
+        logger.info(f"Start scrapping page: {page.url}")
         page_text = page.body_text
         logger.info(page_text)
         logger.info(self.scrapper_schemas)
@@ -29,8 +113,11 @@ class PageScrapper:
                 ),
             },
         ]
-        response_data = await self.model.generate(messages)
-        response_data = json.loads(response_data)
+        response = await self.model.generate(messages)
+        response_data = self.response_processor.process(
+            response=response, page_data=page
+        )
+        logger.info(f"Parsed page: {page.url}\n{response_data}")
         return response_data
 
     # TODO:
@@ -51,8 +138,10 @@ class ScrapperPipeline:
             model=model,
             scrapper_schemas=scrapper_schemas,
         )
+        logger.info(
+            f"ScrapperPipeline start. Scrapping schemas: {scrapper_schemas}",
+        )
 
     async def run(self, url: str):
         page = await Page.parse(url)
         response_data = await self.scrapper.scrape_page(page)
-        logger.info(response_data)
