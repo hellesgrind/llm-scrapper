c689cb0a27975f10ff89ac2716c16170d20b7742
hellesgrind <alekseis.naumov@gmail.com>
2024-04-14 20:33:38 +0200

    add TODOs for conditional adding of nodes LOL

--- app/data_manager.py
+++ app/data_manager.py
@@ -2,6 +2,7 @@ from typing import Dict
 import uuid
 
 from database import DatabaseIntegration
+from llm_client import OpenAIModel
 from schema import (
     PageGraph,
     PageNode,
@@ -16,8 +17,10 @@ class DataManager:
     def __init__(
         self,
         db_integration: DatabaseIntegration,
+        model: OpenAIModel,
     ):
         self.db_integration = db_integration
+        self.model = model
         self.temp_id_to_id: Dict | None = None
 
     async def insert_data(self, data: PageGraph):
@@ -25,9 +28,28 @@ class DataManager:
         nodes = data.nodes
         edges = data.edges
         for node in nodes:
+            # await self.conditional_add_node(node)
             await self.insert_node(node)
         for edge in edges:
             await self.insert_edge(edge)
+
+    # TODO:
+    async def conditional_add_node(self, node: PageNode):
+        existing_nodes = await self.db_integration.get_all_nodes(
+            node_type=node.node_type,
+        )
+
+    # TODO:
+    async def check_if_node_exist(self, node: PageNode):
+        pass
+
+    # TODO:
+    async def create_updated_node(
+        self,
+        existing_node: NodeInfo,
+        new_node: PageNode,
+    ):
+        pass
 
     async def insert_node(self, node: PageNode):
         node_uuid = str(uuid.uuid4())
@@ -39,6 +61,7 @@ class DataManager:
             properties=node.properties,
         )
         await self.db_integration.add_node(node_info)
+        logger.info(f"New node added with data: {node_info}")
 
     async def insert_edge(self, edge: PageEdge):
         from_uuid = self.temp_id_to_id.get(edge.from_temp_id)
@@ -54,3 +77,4 @@ class DataManager:
             type=edge.type,
         )
         await self.db_integration.add_edge(edge_info)
+        logger.info(f"New edge added with data: {edge_info}")
